acc71dfc49a94b51e422fd7c8ced01b059dc4326
STIVEN JOSE SANTANA CRUZ <STIVEN_SANTANA@ucne.edu.do>
2025-02-17 21:21:52 -0500

    Crea modelo

---
+++ Stiven_Santana_P1_AP1/Modelo/Aportes.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stiven_Santana_P1_AP1.Modelo
+{
+    public class Aportes
+    {
+        [Key]
+
+        [Required(ErrorMessage ="Este campo es requerido.")]
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage ="Este campo es requerido.")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage ="Este campo es requerido.")]
+        public string Persona { get; set; }
+
+        [Required(ErrorMessage ="Este campo es requerido.")]
+        public string Observacion { get; set; }
+
+        [Required(ErrorMessage ="Este campo es requerido.")]
+        public decimal Monto { get; set; }
+
+
+    }
+}
